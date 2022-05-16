1baa5bc60d7d9dc78095e9c2e62d42d545f6b636
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-16 19:22:22 +0100

    :rocket: put should do nothig if key is none

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,8 +20,7 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """Adds an item in the cache.
         """
-        if key is None or item is None:
-            return
+        if key and item and key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", last_key)
