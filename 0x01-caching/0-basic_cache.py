7c9ba43493fbbab39b2e9fd6ee370dbe02e4cf6c
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-16 17:03:33 +0100

    :rocket: basic caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,9 +11,8 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Adds an item in the cache.
         """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Retrieves an item by key.
