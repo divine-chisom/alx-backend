24fffedf2ea42200b96bbf9cdba2e26bce52e553
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-19 15:38:51 +0100

    :rocket: most recently used

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -9,7 +9,7 @@ from base_caching import BaseCaching
 class MRUCache(BaseCaching):
     """Represents an object that allows storing and
     retrieving items from a dictionary with an MRU
-    removal mechanism when the limit is reached.
+    algorithm when the limit is reached.
     """
     def __init__(self):
         """Initializes the cache.
