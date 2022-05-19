7f1d2cb71ad5ad219e4f0e78affbfa9560abd391
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-19 10:29:04 +0100

    :rocket: Method put doesn’t do anything if key or item is None

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,11 +20,14 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """Adds an item in the cache.
         """
-        if key and item and key not in self.cache_data:
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", last_key)
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """Retrieves an item by key.
